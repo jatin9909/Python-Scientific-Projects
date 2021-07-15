42b4fb4bc0e3253fad3bd565c33fcbe167e8864a
Jatin <jatin.dhar27@gmail.com>
2021-07-15 22:25:39 +0530

    all materials related to find youtube stats

---
+++ Youtube_stats/stats_graph.py
@@ -0,0 +1,33 @@
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import Data
+df_raw = pd.read_csv("extractdata.csv")
+
+# Prepare Data
+df = df_raw.groupby('UploadYear').size().reset_index(name='counts')
+print(df)
+#print(df) print years with count
+n = df['UploadYear'].unique().__len__() # total 14
+#print(n)
+
+all_colors = list(plt.cm.colors.cnames.keys())
+random.seed(100)
+c = random.choices(all_colors, k=n)
+#print(c)
+
+# Plot Bars
+plt.figure(figsize=(16,10), dpi= 80)
+plt.bar(df['UploadYear'], df['counts'], color=c, width=.6)
+
+for i, val in enumerate(df['counts'].values):
+	plt.text(i, val, '%d'%int(val),horizontalalignment='center', verticalalignment='bottom', fontdict={'fontweight':500, 'size':20})
+
+# Decoration
+
+plt.xticks(df['UploadYear'],fontsize=18)
+plt.title("All YouTube videos with more than 1 Billion views uploads in which year", fontsize=22)
+plt.ylabel('# Number of videoss',fontsize=18)
+plt.ylim(0, 45)
+plt.show()
---
+++ Youtube_stats/youtube_stats.py
@@ -0,0 +1,75 @@
+import pandas as pd
+
+api_key = "AIzaSyDOVaoNBtG1FCpeJmmsFw775RsU7LX_tMM"
+from apiclient.discovery import build
+youtube = build('youtube', 'v3', developerKey=api_key)
+req = youtube.search().list(part='snippet',
+                            q='avengers',
+                            type='video',
+                            maxResults=50)
+
+def get_channel_videos(channel_id):
+    
+    # get Uploads playlist id
+    res = youtube.channels().list(id=channel_id, 
+                                  part='contentDetails').execute()
+  #  playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    
+    videos = []
+    next_page_token = None
+    
+    while 1:
+        res = youtube.playlistItems().list(playlistId='PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC', 
+                                           part='snippet',
+                                           pageToken=next_page_token).execute()
+        videos += res['items']
+        next_page_token = res.get('nextPageToken')
+        
+        if next_page_token is None:
+            break
+    
+    return videos
+
+videos = get_channel_videos('UCEDEKrjFZFp3Br3ENlYomdA')
+video_ids = list(map(lambda x:x['snippet']['resourceId']['videoId'], videos))
+#print(len(video_ids)) will print 507
+
+stats = []
+for i in range(0, len(video_ids)):
+  res = (youtube).videos().list(id=','.join(video_ids[i]),part='statistics').execute()
+  stats += res['items']
+#print(stats)
+print(int((stats['statistics']['viewCount'])))
+# for video in videos:
+#     print(video['snippet']['title'])
+#     print(video['snippet']['publishedAt']) 
+#     print(video['statistics']['viewCount'])
+
+title=[ ]
+liked=[ ]
+disliked=[ ]
+views=[ ]
+url=[ ]
+comment=[ ]
+videoid = []
+publishedDate = []
+video_description = []
+
+for i in range(0,len(video_ids)):
+ # print(i)
+  # i += 1
+ title.append((videos[i])['snippet']['title'])
+ publishedDate.append((videos[i])['snippet']['publishedAt'])
+# print((int((stats[i])['statistics']['viewCount'])))
+ video_description.append((videos[i])['snippet']['description'])
+ liked.append(int((stats[i])['statistics']['likeCount']))
+ disliked.append(int((stats[i])['statistics']['dislikeCount']))
+ views.append(int((stats[i])['statistics']['viewCount']))
+#  comment.append(int((stats[i])['statistics']['commentCount']))
+ videoid.append(videos[i]['snippet']['resourceId']['videoId'])
+
+data={'title':title,'publishedDate':publishedDate}
+df=pd.DataFrame(data)
+print(df)  
+
+df.to_csv('extractdata.csv', index=False)
